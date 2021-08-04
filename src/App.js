23e0a103451bf779022410113d6cdc5ab13bb976
lazarok09 <lazarok09@gmail.com>
2021-08-04 12:04:47 -0300

    feat: useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-function App() {
-  const [reverse, setReverse] = useState('reverse');
+import { useState, useEffect } from 'react';
 
-  function handleReverse() {
-    reverse ? setReverse('') : setReverse('reverse');
-  }
+const showClicks = () => {
+  console.log('h1 clicado');
+};
+function App() {
+  // estados
+  const [counter, setCounter] = useState(0);
+  const handleClick = () => {
+    setCounter((counter) => counter + 1);
+  };
+
+  // componente didMount e o WillUnMount no return
+  useEffect(() => {
+    document.querySelector('h2')?.addEventListener('click', showClicks);
+    console.log('componente montado');
+    return () => {
+      document.querySelector('h2')?.removeEventListener('click', showClicks);
+    };
+  }, []);
+  // component didUpdate
+  useEffect(() => {
+    console.log('algum componente atualizou');
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className={`App-logo ${reverse}`} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-        <button className={'App-button-reverse'} onClick={handleReverse} type="button">
-          click to reverse
-        </button>
-      </header>
+    // component didUpdate
+    <div className={'App'}>
+      <h1>Abre o console</h1>
+      {/* >Mensagem do didmount */}
+      <h2>ComponentDidMount ? : </h2>
+      <p>Contador: {counter}</p>
+      <button type="button" onClick={handleClick}>
+        Counter
+      </button>
+      <p>Altere algum valor aqui e veja o componente sendo montado novamente no console : </p>
     </div>
   );
 }
