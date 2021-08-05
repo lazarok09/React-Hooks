f0d01e4bc2f7131432ddadf967d33c7d246e7fec
lazarok09 <lazarok09@gmail.com>
2021-08-05 10:25:30 -0300

    feat: useCallback

--- src/App.js
+++ src/App.js
@@ -1,47 +1,37 @@
+import P from 'prop-types';
 import './App.css';
-import { useState, useEffect } from 'react';
-
-const showClicks = () => {
-  console.log('h1 clicado');
+import React, { useCallback, useState } from 'react';
+// salva em memória do React o componente botão
+const Button = React.memo(function Button({ incrementButton }) {
+  console.log('Filho, renderizou');
+  return (
+    <button className={'button'} onClick={() => incrementButton(100)}>
+      +
+    </button>
+  );
+});
+// define as tipagens
+Button.propTypes = {
+  incrementButton: P.func,
 };
-function App() {
-  // estados
+export default function App() {
+  // botao
   const [counter, setCounter] = useState(0);
-  const handleClick = () => {
-    setCounter((counter) => counter + 1);
-  };
+  // salva a função em callback para não re-renderizar quando o estado do componente App, (counter) mudar.
+  const incrementFunction = useCallback(
+    // a funçao setCounter usa o callback pra pegar o estado anterior do estado (counter)
+    (number) => {
+      setCounter((prevCounter) => prevCounter + number);
+    },
+    [],
+  );
 
-  // componente didMount e o WillUnMount no return
-  useEffect(() => {
-    document.querySelector('h2')?.addEventListener('click', showClicks);
-    console.log('componente montado');
-    return () => {
-      document.querySelector('h2')?.removeEventListener('click', showClicks);
-    };
-  }, []);
-  // component didUpdate
-  useEffect(() => {
-    console.log('algum componente atualizou');
-  });
-
+  console.log('Pai, renderizou');
   return (
     // component didUpdate
     <div className={'App'}>
-      <h1>Abre o console</h1>
-      {/* >Mensagem do didmount */}
-      <h2>ComponentDidMount ? : </h2>
       <h1>Contador: {counter}</h1>
-      <button className={'button'} type="button" onClick={handleClick}>
-        Counter
-      </button>
-      <ul>
-        {' '}
-        <h1>Ao clicar no botão counter o componente deverá : </h1>
-        <li>componente ser atualizado</li>
-      </ul>
-      <p className={'description'}>Altere algum valor aqui e veja o componente sendo montado novamente no console : </p>
+      <Button incrementButton={incrementFunction} />
     </div>
   );
 }
-
-export default App;
